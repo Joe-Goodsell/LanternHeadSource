4f6e21932e2b4e38b53f58f036785f3296c7d836
katherine-i <winglami@student.unimelb.edu.au>
2023-09-27 20:34:51 +0800

    Added Count Down Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float startTime = 300.0f;
+    private float currentTime;
+    [SerializeField] private TextMeshProUGUI countDown;
+
+    public float CurrentTime {
+        get {
+            return currentTime;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        string currentTimeStr;
+
+        int currentSecond = (int) currentTime % 60;
+        int currentMinute = (int) Math.Floor(currentTime / 60);
+        if (currentMinute >= 1){
+            currentTimeStr = string.Format("{0:00}:{1:00}", currentMinute, currentSecond);
+        } else {
+            currentTimeStr = currentTime.ToString("0.0");
+        }
+
+        countDown.text = currentTimeStr;
+       
+    }
+}
